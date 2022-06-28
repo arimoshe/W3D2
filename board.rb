91f251d4673d8fe4ce4efa4242a272284793abc9
Ari Moshe <arimoshe@gmail.com>
2022-06-28 12:20:55 -0700

    working on memory pre linh commit 12:20pm

--- board.rb
+++ board.rb
@@ -1,6 +1,13 @@
 class Board 
 
+    def initialize
+        @board= Array.new(4) {Array.new(4)}
+    end
 
+    def populate
+        row = rand(0...@board.length)
+        col = rand(0...@board.length)
+        
 
     def 
 end
