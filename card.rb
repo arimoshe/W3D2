57d372d69f626c4e43b5c4bdb52704fb773efd32
Ari Moshe <arimoshe@gmail.com>
2022-06-28 11:48:48 -0700

    working on memory

---
+++ card.rb
@@ -0,0 +1,14 @@
+class Card
+
+    def initialize( face_value)
+        @face_down = true
+        @face_value = face_value
+        @back_value = "🃏"
+    end
+
+    def reveal
+        
+        
+    end
+
+end
