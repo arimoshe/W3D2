3bab61e7f3bf1ac5bfc1adeb4b0935a5705efa66
“NAME” <EMAIL@EMAIL.COM>
2022-06-28 12:06:46 -0700

    12:06 update

--- card.rb
+++ card.rb
@@ -1,14 +1,28 @@
 class Card
 
-    def initialize( face_value)
+    def initialize(face_value)
         @face_down = true
         @face_value = face_value
         @back_value = "🃏"
     end
 
     def reveal
-        
-        
+        @face_down = false if @face_down
+        self
+    end
+    
+    def hide
+        @face_down = true unless @face_down
+         self
+    end
+
+    def to_s
+        @face_value.to_s
+    end
+
+    def ==(value)
+        self.to_s == value.to_s
+       
     end
 
 end
